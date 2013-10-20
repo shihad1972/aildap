b9cc598e90f9b2b8fd466133dbcdd92668d5e4b6
Iain M Conochie <iain@shihad.org>
2013-10-20 21:41:33 +0100

    Start of password encoding routines. Still needs base64 encoding.

--- Makefile
+++ Makefile
@@ -7,11 +7,14 @@ CC = gcc
 RM = rm -f
 ECHO = echo
 CFLAGS = -W -Wall -Wshadow -Wcast-qual -Wwrite-strings -Wunused -D_XOPEN_SOURCE=700 -g
+LDFLAGS = -lcrypto
 bin_PROGRAMS = lsc lcc
 lsc_OBJECTS = ssl-config.o
 lcc_OBJECTS = containers.o
+lcd_OBJECTS = domains.o
+sha_OBJECTS = sha1.c
 
-all:	lsc lcc
+all:	lsc lcc lcd tsha
 
 lcc:	$(lcc_OBJECTS)
 	$(CC) $(CFLAGS) -o $@ $(lcc_OBJECTS)
@@ -19,5 +22,11 @@ lcc:	$(lcc_OBJECTS)
 lsc:	$(lsc_OBJECTS)
 	$(CC) $(CFLAGS) -o $@ $(lsc_OBJECTS)
 
+lcd:	$(lcd_OBJECTS)
+	$(CC) $(CFLAGS) $(LDFLAGS) -o $@ $(lcd_OBJECTS)
+
+tsha:	$(sha_OBJECTS)
+	$(CC) $(CFLAGS) $(LDFLAGS) -o $@ $(sha_OBJECTS)
+
 clean:
-	$(RM) *.o lsc lcc
+	$(RM) *.o lsc lcc lcd tsha
---
+++ sha1.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <openssl/evp.h>
+#include <openssl/sha.h>
+
+int
+main (int argc, char *argv[])
+{
+	EVP_MD_CTX *msg;
+	const EVP_MD *md;
+	char mess[] = "MyP@55w0rd";
+	char type[] = "sha";
+	int i;
+	unsigned char md_value[EVP_MAX_MD_SIZE];
+	unsigned int md_len;
+
+	OpenSSL_add_all_digests();
+	md = EVP_get_digestbyname(type);
+	if (!md) {
+		printf("Unknown digest %s!\n", type);
+		exit(1);
+	}
+	msg = EVP_MD_CTX_create();
+	EVP_DigestInit_ex(msg, md, NULL);
+	EVP_DigestUpdate(msg, mess, strlen(mess));
+	EVP_DigestFinal_ex(msg, md_value, &md_len);
+	EVP_MD_CTX_destroy(msg);
+	for (i = 0; i < SHA_DIGEST_LENGTH; i++)
+		printf("%02x", md_value[i]);
+	printf("\n");
+	printf("\n");
+
+	return 0;
+}
