8da34bb7802ce637f1682ee9342618571ea0df3a
Iain M Conochie <iain@shihad.org>
2013-11-01 11:56:34 +0000

    Updated ssha1.c to use different checksum for the salted hash

--- ssha1.c
+++ ssha1.c
@@ -13,7 +13,7 @@ int
 main()
 {
 	int rd = open("/dev/random", O_RDONLY), i;
-	GChecksum *sum;
+	GChecksum *sum, *nsum;
 	gchar *pass;
 	gsize slen = 20;
 	const guchar mypass[] = "MyPa55w0rd", salt[6];
@@ -30,18 +30,20 @@ main()
 	sum = g_checksum_new(G_CHECKSUM_SHA1);
 	g_checksum_update(sum, mypass, -1);
 	g_checksum_get_digest(sum, output, &slen);
+	g_checksum_free(sum);
 	pass = g_base64_encode(output, 20);
 	printf("{SHA}%s\n", pass);
 	g_free(pass);
-	sum = g_checksum_new(G_CHECKSUM_SHA1);
-	g_checksum_update(sum, mypass, -1);
-	g_checksum_update(sum, salt, 6);
-	g_checksum_get_digest(sum, output, &slen);
+	nsum = g_checksum_new(G_CHECKSUM_SHA1);
+	g_checksum_update(nsum, mypass, -1);
+	g_checksum_update(nsum, salt, 6);
+	g_checksum_get_digest(nsum, output, &slen);
 	for (i = 0; i < 6; i++)
 		*(output + 20 + i) = salt[i];
 	pass = g_base64_encode(output, 26);
 	printf("{SSHA}%s\n", pass);
 	g_free(pass);
+	g_checksum_free(nsum);
 	free(output);
 	return 0;
 }
