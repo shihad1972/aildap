a310c5f02545eeec2c6dac79ccd3eddfd69b5b70
Iain M Conochie <iain@shihad.org>
2014-03-29 00:10:44 +0000

    We can now create the db for a new domain with lcdb

--- lcdb.c
+++ lcdb.c
@@ -41,20 +41,105 @@ parse_lcdb_command_line(int argc, char *argv[], lcdb_s *data)
 	if (!(data))
 		return ONE;
 	while ((opt = getopt(argc, argv, "a:d:p:f")) != -1) {
-		;
+		if (opt == 'a')
+			check_snprintf(data->admin, NAME, optarg, "data->admin");
+		else if (opt == 'd')
+			check_snprintf(data->domain, DOMAIN, optarg, "data->domain");
+		else if (opt == 'p')
+			check_snprintf(data->dir, DOMAIN, optarg, "data->dir");
+		else if (opt == 'f')
+			data->file = 1;
+	}
+	if ((strlen(data->admin) == 0) || (strlen(data->domain) == 0)) {
+		rep_usage(argv[0]);
+		return WARG;
 	}
 	return retval;
+}
+
+void
+output_db_ldif(lcdb_s *data)
+{
+	char *ldf, *dir, *dom, *adm, *hsh;
+	int retval = NONE;
+	size_t len, dlen;
+	FILE *out;
+
+	if (!(data)) {
+		fprintf(stderr, "null pointer passed to output_db_ldif\n");
+		exit(1);
+	}
+	dom = data->domain;
+	dir = data->dir;
+	adm = data->admin;
+	hsh = data->phash;
+	dlen = strlen(dom);
+	len = strlen(data->dir);
+	ldf = get_ldif_domain(dom);
+	if (len > 0) {
+		if ((retval = add_trailing_slash(data->dir)) < 0) {
+			fprintf(stderr, "Can't add trailing slash to dir %s\n",
+				data->dir);
+			exit(BUFF);
+		}
+		strncat(data->dir, dom, dlen);
+	} else {
+		snprintf(data->dir, DN, "/var/lib/slapd/%s", dom);
+	}
+	if (data->file > 0) {
+		if (!(out = fopen("db.ldif", "w"))) {
+			fprintf(stderr, "Cannot write to db.ldif\n");
+			exit(FILE_O_FAIL);
+		}
+	} else {
+		out = stdout;
+	}
+	fprintf(out, "\
+# %s domain, hdb, config\n\
+dn: olcDatabase=hdb,cn=config\n\
+objectClass: olcDatabaseConfig\n\
+objectClass: olcHdbConfig\n\
+olcDatabase: hdb\n\
+olcDbDirectory: %s\n\
+olcSuffix: %s\n\
+olcAccess: to attrs=userPassword,shadowLastChange by self write by \
+anonymous auth by dn=\"cn=%s,%s\" write by * none\n\
+olcAccess: to dn.base="" by * read\n\
+olcAccess: to * by self write by dn=\"cn=%s,%s\" write by * read\n\
+olcRootDN: cn=%s,%s\n\
+olcRootPW: {SSHA}%s\n\
+olcDbCheckpoint: 512 30\n\
+olcDbConfig: set_cachesize 0 2097152 0\n\
+olcDbConfig: set_lk_max_objects 1500\n\
+olcDbConfig: set_lk_max_locks 1500\n\
+olcDbConfig: set_lk_max_lockers 1500\n\
+olcDbIndex: default pres,eq\n\
+olcDbIndex: uid\n\
+olcDbIndex: cn,sn pres,eq,sub\n\
+olcDbIndex: objectClass eq\n\
+olcDbIndex: uniqueMember eq\n\
+olcDbIndex: uidNumber,gidNumber pres,eq\n",
+dom, dir, ldf, adm, ldf, adm, ldf, adm, ldf, hsh);
+	if (out != stdout)
+		fclose(out);
 }
 
 int
 main (int argc, char *argv[])
 {
-	char *pass, *phash;
+	int retval = NONE;
+	lcdb_s *data = '\0';
 
-	pass = getPassword("Enter password for admin DN: ");
-	phash = get_ldif_pass_hash(pass);
-	printf("{SSHA}%s\n", phash);
-	free(phash);
-	free(pass);
-	return NONE;
+	if (!(data = malloc(sizeof(lcdb_s))))
+		rep_error("data");
+	init_lcdb_data_struct(data);
+	if ((retval = parse_lcdb_command_line(argc, argv, data)) != 0) {
+		clean_lcdb_data(data);
+		return retval;
+	}
+	data->pass = getPassword("Enter password for admin DN: ");
+	data->phash = get_ldif_pass_hash(data->pass);
+	output_db_ldif(data);
+	clean_lcdb_data(data);
+	return retval;
 }
