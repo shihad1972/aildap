caa2f60d5edb045b7a5dcd84c9287aa07f039f87
Iain M Conochie <iain@thargoid.co.uk>
2014-04-13 19:28:47 +0100

    Moved containers.c to lcc.c

--- src/lcc.c
+++ src/lcc.c
@@ -16,7 +16,7 @@
  *  You should have received a copy of the GNU General Public License
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
- *  containers.c:
+ *  lcc.c:
  *
  *  Main file for lcc program: ldap create containers
  */
