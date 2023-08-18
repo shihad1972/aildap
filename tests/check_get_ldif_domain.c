09df0e42f5d1f51e7fbbed0db3080061f1802e7c
Iain M Conochie <iain@thargoid.co.uk>
2023-08-18 23:47:39 +0100

    Added full test

--- tests/check_get_ldif_domain.c
+++ tests/check_get_ldif_domain.c
@@ -1,3 +1,14 @@
+START_TEST(test_get_ldif_domain)
+{
+        char *domain = "ailsatech.net", *dn;
+
+        dn = get_ldif_domain(domain);
+        ck_assert_str_eq(domain, "dc=ailsatech,dc=net")
+        free(dn);
+
+}
+END_TEST
+
 int main(void)
 {
         return 0;
