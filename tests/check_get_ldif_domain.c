76d8a0b7b1a9c4bf0327bf05c82c077c3b527d19
Iain M Conochie <iain@thargoid.co.uk>
2023-08-18 23:58:00 +0100

    Extended test

--- tests/check_get_ldif_domain.c
+++ tests/check_get_ldif_domain.c
@@ -1,13 +1,22 @@
+#include <stdlib.h>
+#include <string.h>
 #include <check.h>
 #include "../include/ailsaldap.h"
 START_TEST(test_get_ldif_domain)
 {
-        char *domain = "ailsatech.net", *dn;
+        char *domain = "ailsatech.net", *freak = "you.me.them.us.whatever.com", *dn;
+        size_t len;
 
         dn = get_ldif_domain(domain);
         ck_assert_str_eq(domain, "dc=ailsatech,dc=net");
+        len = strlen(dn):
+        memset(dn, '\0', len);
         free(dn);
-
+        dn = get_ldif_domain(freaky);
+        ck_assert_str_eq(domain, "dc=you,dc=me,dc=them,dc=us,dc=whatever,dc=com"
+        len = strlen(dn):
+        memset(dn, '\0', len);
+        free(dn);
 }
 END_TEST
 
