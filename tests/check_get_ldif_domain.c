f6e03acb385271ab4d16f9c938c7ea1ab42aacba
Iain M Conochie <iain@thargoid.co.uk>
2023-08-18 23:51:54 +0100

    Fixed syntax error

--- tests/check_get_ldif_domain.c
+++ tests/check_get_ldif_domain.c
@@ -5,7 +5,7 @@ START_TEST(test_get_ldif_domain)
         char *domain = "ailsatech.net", *dn;
 
         dn = get_ldif_domain(domain);
-        ck_assert_str_eq(domain, "dc=ailsatech,dc=net")
+        ck_assert_str_eq(domain, "dc=ailsatech,dc=net");
         free(dn);
 
 }
